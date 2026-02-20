e8e9f120cc86cd36800d12185288249025e7add2
MeArmandP <apeters@icstars.org>
2026-02-20 13:03:31 -0600

    katas loops

--- js-katas/katas/loops.js
+++ js-katas/katas/loops.js
@@ -4,8 +4,7 @@ function firstNEven(n = 10) {
   // TODO: write your solution here (replace the line below).
   // Return an array of the first n even numbers (starting at 2).
   // Example: firstNEven(5) -> [2, 4, 6, 8, 10]
-  // Explain your logic choices in comments as you code.
- function firstneven(2) {
+  // Explain your logic choices in comments as you code. {
   const result = [];
   for (let i =1; <= n; i++){
     result.push(i*2);
